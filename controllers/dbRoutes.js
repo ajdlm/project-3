5defa35c97fc5e828e9501db6c7d981e08b177af
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2019-12-17 16:24:46 -0600

    Fixed a problem with how changes to dbRoutes.js had been merged into master, also updated dummyWork.json to reflect the changes to the Work schema

--- controllers/dbRoutes.js
+++ controllers/dbRoutes.js
@@ -4,40 +4,37 @@ const router = express.Router();
 
 const Work = require("../models/Work.js");
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 964d43ffa7304c90444fc5e88e3e0d349db12344
 //Route for Curation Data
 router.get("/api/work", function(req, res) {
-  Work.find({series: "curation"})
+  Work.find({ series: "curation" })
     .then(function(works) {
       res.json(works);
-    }).catch(function(err) {
+    })
+    .catch(function(err) {
       res.json(err);
     });
 });
 
-<<<<<<< HEAD
 //Route for Painting Data
 router.get("/api/paint", function(req, res) {
-  Work.find({isPaint: true})
+  Work.find({ isPaint: true })
     .then(function(works) {
       res.json(works);
-    }).catch(function(err) {
-    res.json(err);
-  });
-});
-=======
-//Route for finding by series
-router.get("/api/work/:series", function(req, res) {
-  Work.find({series: req.params.series})
-    .then(works => {
-      res.json(works);
-    }).catch(function(err) {
+    })
+    .catch(function(err) {
       res.json(err);
     });
 });
 
->>>>>>> 964d43ffa7304c90444fc5e88e3e0d349db12344
+//Route for finding by series
+router.get("/api/work/:series", function(req, res) {
+  Work.find({ series: req.params.series })
+    .then(works => {
+      res.json(works);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
 module.exports = router;
